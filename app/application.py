67db8dd43ebee675f425bc91f935765c5833a6bd
Spectre-ak <62694340+Spectre-ak@users.noreply.github.com>
2021-10-07 12:18:18 +0530

    Update application.py

--- app/application.py
+++ app/application.py
@@ -2,6 +2,7 @@ from flask import Flask, json,render_template, request, jsonify
 from flask.helpers import send_file
 import run_model_memory
 import uuid
+import db_util
 
 application = Flask(__name__)
 
@@ -33,10 +34,28 @@ def upload_file():
         print(name)
         f.save((name))
         try:
-            result=run_model_memory.run(name,id)
+            result=run_model_memory.run(name,id,True)
             return jsonify(result)
         except Exception as e:
+            print(e)
             return jsonify([])
+
+@application.route('/upload_and_store', methods = ['GET', 'POST'])
+def upload_s3():
+    if request.method == 'POST':
+        f = request.files['file']
+        id=str(uuid.uuid4())
+        name=id+".jpg"
+        print(name)
+        f.save((name))
+        try:
+            tags=run_model_memory.run(name,id,False)
+            db_util.save_image_details(name,tags)
+            return jsonify({"error":False})
+        except Exception as e:
+            print(e)
+            return jsonify({"error":True})
+
 
 
 if __name__ == "__main__":
