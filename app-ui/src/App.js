3b2f3a4a6c4938c36ecc8fa8d1224308c52be675
Spectre-ak <62694340+Spectre-ak@users.noreply.github.com>
2021-10-09 22:40:51 +0530

    ui files

---
+++ app-ui/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state ={};
+  }
+  render(){
+    return(
+      <div>j</div>
+    )
+  }
+}
+
+export default App;
