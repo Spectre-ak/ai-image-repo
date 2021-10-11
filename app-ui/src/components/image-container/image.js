cd00226cd33abcfa4ce58fa41b3832e33e994c48
Spectre-ak <62694340+Spectre-ak@users.noreply.github.com>
2021-10-12 01:57:43 +0530

    Update image.js

--- app-ui/src/components/image-container/image.js
+++ app-ui/src/components/image-container/image.js
@@ -5,35 +5,35 @@ class ImageComponent extends React.Component {
     constructor(props) {
         super(props);
     }
+    
 
     render() {
         return (
             <div class="row">
-                <ImageColDefComponent/>
-                <ImageColDefComponent/>
-                <ImageColDefComponent/>
-                <ImageColDefComponent/>
-                <ImageColDefComponent/>
-                <ImageColDefComponent/>
-                
+                <ImageColDefComponent />
+                <ImageColDefComponent />
+                <ImageColDefComponent />
+                <ImageColDefComponent />
+                <ImageColDefComponent />
+                <ImageColDefComponent />
             </div>
         )
     }
 }
 
-function ImageColDefComponent() {
-    <div class="col-md-3 col-sm-4">
-        <div class="thumbnail" href="#">
-            <a href="#">
-                <img class="img-responsive" src="http://placehold.it/175x266" alt="" />
-            </a>
-            <div class="caption">
-                <a href="#" class="btn btn-shopping btn-responsive">
-                    adfas
-                </a>
+function ImageColDefComponent(props) {
+    return (
+        <div className="col-md-3 col-sm-4">
+            <div className="thumbnail" href="#">
+                <span>
+                    <img className="img-fluid" src="http://placehold.it/175x266" alt="" />
+                </span>
+                <div class="caption">
+                    tags goes here
+                </div>
             </div>
         </div>
-    </div>
+    )
 };
 
-export default ImageColDefComponent;
+export default ImageComponent;
