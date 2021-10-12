c5a1c7f6d1bddad965a51d10286ce71e53391b73
Spectre-ak <62694340+Spectre-ak@users.noreply.github.com>
2021-10-13 00:19:22 +0530

    video frames test

--- app/run_model_memory.py
+++ app/run_model_memory.py
@@ -4,6 +4,12 @@ import tensorflow as tf
 from PIL import Image
 from object_detection.utils import label_map_util
 import cv2
+import uuid
+from operator import getitem
+import json
+import threading
+import time
+import shutil
 
 PATH_TO_FROZEN_GRAPH = 'frozen_inference_graph.pb'
 PATH_TO_LABELS = 'object_detection/data/mscoco_label_map.pbtxt'
@@ -24,11 +30,20 @@ with detection_graph.as_default():
 detection_graph.as_default()
 sess=tf.compat.v1.Session(graph=detection_graph)
 # print(category_index)
+# print(categories)
+# cat = []
+# for f in categories:
+#     cat.append(f['name'])
+# print(cat)
 
-def run(image_path,id,flagForStore):
-    img=cv2.imread(image_path)
-    #img=img.resize((1280,720))
-    image_np = np.array(img)
+def run(image_path, id, flagForStore, image_frame_process=None, image_frame_process_flag = False):
+
+    if image_frame_process_flag:
+        image_np = np.array(image_frame_process)
+    else:
+        img = cv2.imread(image_path)
+        image_np = np.array(img)
+    
     print(image_np.shape)
     image_np_expanded = np.expand_dims(image_np, axis=0)
     print(image_np_expanded.shape)
@@ -42,17 +57,103 @@ def run(image_path,id,flagForStore):
     (boxes, scores, classes, num_detections) = sess.run(
         [boxes, scores, classes, num_detections],
         feed_dict={image_tensor: image_np_expanded})
-    # print((classes))
+    # print(classes)
     # print(scores)
     detected_objs = []
     for index in range(0,len(scores[0])):
         if scores[0][index]==0:
             break
         detected_objs.append(category_index[classes[0][index]]['name'])
-    print(set(detected_objs))
+    # print(set(detected_objs))
     detected_objs=list(set(detected_objs))
     print(detected_objs)
     if flagForStore:
         os.remove(image_path)
     return detected_objs
     
+
+def run_video(process_details):
+    
+    target_objs = []
+    if process_details['type'] == "img":
+        target_objs = run(process_details['image_path'],process_details['temp_dir_id'], False)
+    else:
+        target_objs = json.loads(process_details['search_text'])
+    
+    print("targetting objects are ")
+    print(target_objs)
+
+    count = 0
+    count_Seconod = 0
+    
+    similar_frames_res = {}
+    
+    cap=cv2.VideoCapture(process_details['video_path'])
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    print(fps)
+    
+    while cap.isOpened():
+        ret, frame = cap.read()
+        print(count)
+        print(count_Seconod)
+        count_Seconod+=1
+        if ret:
+            found_objs_in_frame = run('', '', False, image_frame_process=frame, image_frame_process_flag = True)
+            similar_objs_found_in_frame = []
+
+            for target_obj in target_objs:
+                if target_obj in found_objs_in_frame:
+                    similar_objs_found_in_frame.append(target_obj)
+
+            score=len(similar_objs_found_in_frame)
+            print('similar objs found00')
+            print(similar_objs_found_in_frame)
+
+            if score > 0:
+                frame_id = str(uuid.uuid4())
+                frame_path = 'static/'+process_details['temp_dir_id']+'/'+frame_id+'.jpg'
+                frame_obj = {
+                    "frame_path":frame_path, 
+                    "similar_objs":similar_objs_found_in_frame,
+                    "score":score
+                    }
+                similar_frames_res[frame_id] = frame_obj
+                cv2.imwrite(frame_path, frame)
+
+            count += int(fps) # i.e. at 30 fps, this advances one second
+            cap.set(1, count)
+        else:
+            cap.release()
+            break
+    
+    print(similar_frames_res)
+
+    similar_frames_res = { k: v for k, v in sorted(similar_frames_res.items(), key=lambda item: getitem(item[1], 'score'), reverse=True) }
+    
+    print(similar_frames_res)
+
+    print("converting dic to list")
+    reorder_protected_json = []
+    for k,v in similar_frames_res.items():
+        print(k)
+        print(v)
+        reorder_protected_json.append({k:v})
+    print(reorder_protected_json)
+    print("intiating os remove dir thread")
+    thread_remove_dir = thread(temp_dir_id=process_details['temp_dir_id'])
+    thread_remove_dir.start()
+    shutil.make_archive("static/"+process_details['temp_dir_id'], 'zip', "static/"+process_details['temp_dir_id'])
+    return reorder_protected_json
+
+   
+
+class thread(threading.Thread):
+    def __init__(self, temp_dir_id,):
+        threading.Thread.__init__(self)
+        self.temp_dir_id = temp_dir_id
+    def run(self):
+        time.sleep(2700)
+        os.remove("static/"+self.temp_dir_id+".zip")
+        shutil.rmtree("static/"+self.temp_dir_id)
+
+
