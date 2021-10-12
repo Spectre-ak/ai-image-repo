2d6b1005eba7d7ba9576caaff819687828fd6e28
Spectre-ak <62694340+Spectre-ak@users.noreply.github.com>
2021-10-12 11:13:30 +0530

    Update image.js

--- app-ui/src/components/image-container/image.js
+++ app-ui/src/components/image-container/image.js
@@ -1,21 +1,33 @@
 import React from "react";
+import { Loader } from "../home-repo/home-repo";
 import "./styles.css"
 
 class ImageComponent extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            renderData:<Loader/>
+        };
     }
-    
 
+    componentDidMount(){
+        console.log(this.props.data);
+        const processedComps=[];
+        this.props.data.forEach(element => {
+            const tags=[];
+            element.tags.forEach(elementTag => {
+                tags.push(<ImageTagsComponent tag={elementTag}/>);
+            });
+            processedComps.push(<ImageColDefComponent src={element.img} tags={tags}/>)
+        });
+        this.setState({
+            renderData:processedComps
+        });
+    }
     render() {
         return (
             <div class="row">
-                <ImageColDefComponent />
-                <ImageColDefComponent />
-                <ImageColDefComponent />
-                <ImageColDefComponent />
-                <ImageColDefComponent />
-                <ImageColDefComponent />
+                {this.state.renderData}
             </div>
         )
     }
@@ -26,14 +38,22 @@ function ImageColDefComponent(props) {
         <div className="col-md-3 col-sm-4">
             <div className="thumbnail" href="#">
                 <span>
-                    <img className="img-fluid" src="http://placehold.it/175x266" alt="" />
+                    <img className="img-fluid" src={props.src} alt="" />
                 </span>
                 <div class="caption">
-                    tags goes here
+                    {props.tags}
                 </div>
             </div>
         </div>
     )
 };
 
+function ImageTagsComponent(props){
+    return(
+        <span style={{paddingRight:"2px", paddingLeft:"2px"}}>
+            <span class="badge badge-primary" >{props.tag}</span>
+        </span>
+        
+    )
+}
 export default ImageComponent;
