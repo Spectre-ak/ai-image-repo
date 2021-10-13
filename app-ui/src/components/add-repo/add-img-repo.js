de47579ff436e95225ad51b3c4ff9b6e06af4e0e
Spectre-ak <62694340+Spectre-ak@users.noreply.github.com>
2021-10-13 15:17:35 +0530

    defined image upload and search components

--- app-ui/src/components/add-repo/add-img-repo.js
+++ app-ui/src/components/add-repo/add-img-repo.js
@@ -1,12 +1,173 @@
 import React from "react";
+import { Loader } from "../home-repo/home-repo";
+import ImageComponent from "../image-container/image";
+import { CustomAlertComponent } from "../../App";
+import ContentFunctionalityStatus from "../nav/status";
+import { VideoProcessingDone } from "../video-search/video-search";
 
-class AddImageRepoComponent extends React.Component{
-    constructor(props){
+const AddImagesToRepoTitle = () => {
+    return (
+        <div class="d-flex justify-content-center">
+            <big>
+                <b>Contribute images to AI Image Repository</b>
+            </big>
+        </div>
+    )
+}
+const workingStatus = <span>Working <i class="fa fa-check" aria-hidden="true" style={{ color: "lime" }}></i></span>;
+const ImgUploadAndProcessStatus = () => {
+    return (
+        <span> &nbsp;Uploading and classifying your images &nbsp;
+            <span class="spinner-border spinner-border-sm" style={{ color: "#FF9800!important" }} role="status" aria-hidden="true"></span></span>
+    )
+};
+
+
+class AddImageRepoComponent extends React.Component {
+    constructor(props) {
         super(props);
+        this.state = {
+            isAvailableStatus: <span style={{ color: "#fff" }}>Pending <span class="spinner-border spinner-border-sm" style={{ color: "#FF9800!important" }} role="status" aria-hidden="true"></span></span>,
+            buttonDisabled:false,
+            renderAddImagesToRepo:[],
+            messageSuccess:"",
+            imgUploadAndProcessStatus:"",
+            alertMessage:"",
+        }
+        this.updateContentStatus = this.updateContentStatus.bind(this);
+        this.onFormSubmit=this.onFormSubmit.bind(this);
+        
+        this.showPopupMessage=this.showPopupMessage.bind(this);
     }
-    render(){
-        return(
-            <h1>add imgs to repo component is here</h1>
+    componentDidMount() {
+        this.props.setContentStatus(<ContentFunctionalityStatus status={this.state.isAvailableStatus} />);
+        this.props.setComponentTitle(<AddImagesToRepoTitle />);
+        fetch("http://localhost:5000/upload_and_store_status")
+            .then(res => res.json())
+            .then(res => {
+                console.log(res);
+                if (res.status) {
+                    this.props.setContentStatus(<ContentFunctionalityStatus status={workingStatus} />);
+                }
+                else{
+                    this.updateContentStatus();
+                }
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
+    updateContentStatus() {
+        const intervalIdForCheckingUploadStatus = setInterval(async () => {
+            console.log("executing inteval");
+            await fetch("http://localhost:5000/upload_and_store_status")
+                .then(res => res.json())
+                .then(res => {
+                    console.log(res);
+                    if (res.status) {
+                        console.log("cleaering interbval");
+                        this.props.setContentStatus(<ContentFunctionalityStatus status={workingStatus} />);
+                        clearInterval(intervalIdForCheckingUploadStatus);
+                    }
+                })
+                .catch(err => {
+                    console.log(err);
+                    console.log("cleaering interbval");
+                    clearInterval(intervalIdForCheckingUploadStatus);
+                });
+
+        }, 10000);
+    }
+    onFormSubmit(event){
+        event.preventDefault();
+        console.log(event);
+        console.log(event.target[0].files);
+        console.log(event.target[0].files.length);
+
+        const files=event.target[0].files;
+        const filesLength=files.length;
+
+        if(filesLength>30){
+            this.showPopupMessage("Max 30 images allowed at a time");
+            return;
+        }
+        for(let index=0;index<filesLength;index++){
+            let size = files[index].size / 1024 / 1024;
+            size = parseInt(size);
+            if(!files[index].type.includes("image") || size>=10){
+                this.showPopupMessage("Files must be of type image(png or jpg) and size less than 10MB");
+                return;
+            }
+        }
+        
+        const reqData =new FormData(event.target);
+        this.setState({
+            buttonDisabled:true,
+            renderAddImagesToRepo:<Loader/>,
+            imgUploadAndProcessStatus:<ImgUploadAndProcessStatus/>
+        });
+        fetch('http://localhost:5000/upload_and_store', {
+            method: 'POST',
+            body: reqData,
+        }).then(res=>res.json()).then(res=>{
+            console.log(res);
+            if(!res.error){
+                this.setState({
+                    buttonDisabled:false,
+                    renderAddImagesToRepo:<ImageComponent data={res.obj}/>,
+                    messageSuccess:"Added following images successfully",
+                    imgUploadAndProcessStatus:<VideoProcessingDone/>
+                });
+            }
+            else{
+                this.updateUserErrorMessage();
+            }
+        }).catch(err=>{
+            console.log(err);
+            this.updateUserErrorMessage();
+        });
+    }
+    updateUserErrorMessage(){
+        this.setState({
+            buttonDisabled:false,
+            messageSuccess:"Some error occued while saving processing you images and saving. Try again later.",
+            imgUploadAndProcessStatus:"",
+            renderAddImagesToRepo:[]
+        });
+    }
+    showPopupMessage(message){
+        this.setState({
+            alertMessage:message
+        },()=>{
+            document.getElementById("custom-alert-msg-modal").click();
+        });
+
+    }
+    render() {
+        return (
+            <div className="container">
+                <b>Select images to add to repo</b>
+                <br/><br/>
+                <form onSubmit={this.onFormSubmit}>
+                    Select images: &nbsp; &nbsp;
+                    <input type="file" name="file" required accept="image/*" multiple/>
+                    <br/><br/>
+                    <button type="submit" class="btn btn-outline-primary" disabled={this.state.buttonDisabled}>
+                        <i class="fa fa-upload" aria-hidden="true"></i> Save Images
+                    </button>
+                    <span>
+                        {this.state.imgUploadAndProcessStatus}
+                    </span>
+                </form>
+
+                <br/>
+                <h5>{this.state.messageSuccess}</h5>
+                <br/>
+                {this.state.renderAddImagesToRepo}
+
+
+                <CustomAlertComponent message={this.state.alertMessage} />
+            </div>
         )
     }
 }
--- app-ui/src/components/image-search/image-search.js
+++ app-ui/src/components/image-search/image-search.js
@@ -1,14 +1,141 @@
 import React from "react";
+import { CustomAlertComponent } from "../../App";
+import { Loader } from "../home-repo/home-repo";
+import ImageComponent from "../image-container/image";
+import { ClassifyImageTitleComponent, ImgUploadAndProcessStatus, Status } from "../image-test/image-test";
+import ContentFunctionalityStatus from "../nav/status";
+import { VideoProcessingDone } from "../video-search/video-search";
+
 
 class ImageSearchComponent extends React.Component{
     constructor(props){
         super(props);
+        this.state={
+            imgUploadAndProcessStatus:"",
+            renderAddImagesToRepo:[],
+            messageSuccess:"",
+            alertMessage:"",
+            buttonDisabled:false,
+
+        }
+        this.onFormSubmit=this.onFormSubmit.bind(this);
+        this.showPopupMessage=this.showPopupMessage.bind(this);
+        this.updateUserErrorMessage=this.updateUserErrorMessage.bind(this);
+    }
+    componentDidMount(){
+        this.props.setContentStatus(<ContentFunctionalityStatus status={Status} />);
+        this.props.setComponentTitle(<ClassifyImageTitleComponent message={"Search in the repository using image"}/>);
+    }
+    onFormSubmit(event){
+        event.preventDefault();
+        console.log(event);
+        console.log(event.target[0].files);
+        console.log(event.target[0].files.length);
+
+        const files=event.target[0].files;
+        const filesLength=files.length;
+
+        if(filesLength>1){
+            this.showPopupMessage("Max 10 images can be processed at a time");
+            return;
+        }
+        for(let index=0;index<filesLength;index++){
+            let size = files[index].size / 1024 / 1024;
+            size = parseInt(size);
+            if(!files[index].type.includes("image") || size>=10){
+                this.showPopupMessage("Files must be of type image(png or jpg) and size less than 10MB");
+                return;
+            }
+        }
+        const reqData =new FormData(event.target);
+        this.setState({
+            buttonDisabled:true,
+            renderAddImagesToRepo:<Loader/>,
+            imgUploadAndProcessStatus:<ImgUploadAndProcessStatus message="Uploading and searching for similar images in the repo"/>
+        });
+
+        fetch('http://localhost:5000/search_image', {
+            method: 'POST',
+            body: reqData,
+        }).then(res=>res.json()).then(res=>{
+            console.log(res);
+            if(!res.error){
+                const processDataForImageComponent=[];
+                res.obj.forEach(element=>{
+                    processDataForImageComponent.push(
+                        {
+                            "img":Object.keys(element)[0],
+                            "tags":element[Object.keys(element)[0]].objs
+                        }
+                    )
+                });
+                console.log(processDataForImageComponent);
+                if(processDataForImageComponent.length!==0)
+                    this.setState({
+                        buttonDisabled:false,
+                        renderAddImagesToRepo:<ImageComponent data={processDataForImageComponent}/>,
+                        messageSuccess:"Similar images found",
+                        imgUploadAndProcessStatus:<VideoProcessingDone/>
+                    });
+                else
+                    this.setState({
+                        buttonDisabled:false,
+                        renderAddImagesToRepo:[],
+                        messageSuccess:"No images found with the provided search image",
+                        imgUploadAndProcessStatus:"",
+                    });
+            }
+            else{
+                this.updateUserErrorMessage();
+            }
+        }).catch(err=>{
+            console.log(err);
+            this.updateUserErrorMessage();
+        });
+
+    }
+    updateUserErrorMessage(){
+        this.setState({
+            buttonDisabled:false,
+            messageSuccess:"Some error occued while saving processing you image. Try again later.",
+            imgUploadAndProcessStatus:"",
+            renderAddImagesToRepo:[]
+        });
+    }
+    showPopupMessage(message){
+        this.setState({
+            alertMessage:message
+        },()=>{
+            document.getElementById("custom-alert-msg-modal").click();
+        });
+
     }
     render(){
         return(
-            <h1>Image  search component is here</h1>
+            <div>
+                <b>Image  search component is here</b>
+                <br /><br />
+                <form onSubmit={this.onFormSubmit}>
+                    Select image: &nbsp; &nbsp;
+                    <input type="file" name="file" required accept="image/*" />
+                    <br /><br />
+                    <button type="submit" class="btn btn-outline-primary" disabled={this.state.buttonDisabled}>
+                        <i class="fa fa-upload" aria-hidden="true"></i> Upload Image
+                    </button>
+                    <span>
+                        {this.state.imgUploadAndProcessStatus}
+                    </span>
+                </form>
+                <br/>
+                <h5>{this.state.messageSuccess}</h5>
+                {this.state.renderAddImagesToRepo}
+
+                <br />
+                <CustomAlertComponent message={this.state.alertMessage} />
+            </div>
         )
     }
 }
+
 
 export default ImageSearchComponent;
